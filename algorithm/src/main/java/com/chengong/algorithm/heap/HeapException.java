ea5a771ee430517bcf741571e1d6fa38c844c0f4
rgc588 <gongchen822@gmail.com>
2014-04-22 10:35:47 -0400

    add heap

---
+++ algorithm/src/main/java/com/chengong/algorithm/heap/HeapException.java
@@ -0,0 +1,9 @@
+package com.chengong.algorithm.heap;
+
+/**
+ * Created by chengong on 4/11/14.
+ */
+public class HeapException extends RuntimeException {
+    public HeapException(){};
+    public HeapException(String msg){super(msg);}
+}
