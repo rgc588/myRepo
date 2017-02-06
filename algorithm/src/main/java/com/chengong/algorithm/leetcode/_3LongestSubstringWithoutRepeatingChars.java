fc30a4a5572cccbc6b77ef7067e778316bf2fff0
cheng <cheng@nvidia.com>
2017-02-06 13:03:46 -0800

    add leetcode #3

---
+++ algorithm/src/main/java/com/chengong/algorithm/leetcode/_3LongestSubstringWithoutRepeatingChars.java
@@ -0,0 +1,24 @@
+package com.chengong.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by cheng on 2/6/2017.
+ */
+public class _3LongestSubstringWithoutRepeatingChars {
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int cur = -1;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c) && cur < map.get(c)) {
+                cur = map.get(c);
+            }
+            max = max > i - cur ? max : i - cur;
+            map.put(c, i);
+        }
+        return max;
+    }
+}
